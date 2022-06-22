1279655cb5a1dcb39ebe224b593aa6e8659ea7e2
Eiichiro Ito <eiichiro.ito@gmail.com>
2022-06-22 15:46:28 +0900

    add Examples.

--- Modules/exboard.py
+++ Modules/exboard.py
@@ -1,46 +1,53 @@
+from microbit import i2c
 import microbit as mb
 
-class DFRobotExBoard():
-    def __init__(self, addr=64):
-        self.addr = addr
-        mb.i2c.write(self.addr,bytes([0,0]))
-        oldmode = (mb.i2c.read(self.addr,1))[0]
-        newmode = ((oldmode & 127) | 16)
-        mb.i2c.write(self.addr,bytes([0,newmode]))
-        mb.i2c.write(self.addr,bytes([254,121]))
-        mb.i2c.write(self.addr,bytes([0,oldmode]))
-        mb.sleep(5)
-        mb.i2c.write(self.addr,bytes([0,(oldmode | 161)]))
-        
-    def servo(self, ch, deg):
-        tmp = round(((((deg * 10) + 600) * 4095) / 20000))
-        pxt = []
-        pxt.append((6 + (4 * (16 - ch))))
-        pxt.append(0)
-        pxt.append(0)
-        pxt.append((tmp & 255))
-        pxt.append(((int(tmp) >> int(8)) & 255))
-        mb.i2c.write(self.addr,bytes(pxt))
+address=None
 
-    def motor(self, ch, speed):
-        tmp = speed * 16
-        tmp = min(tmp,4095)
-        tmp = max(tmp,-4095)
-        tmp2 = 0
-        if (tmp < 0):
-            tmp2 = -tmp
-            tmp = 0
-        pxt = []
-        pxt.append((6 + (4 * (((4 - ch) * 2) + 1))))
-        pxt.append(0)
-        pxt.append(0)
-        pxt.append((tmp & 255))
-        pxt.append(((int(tmp) >> int(8)) & 255))
-        mb.i2c.write(self.addr,bytes(pxt))
-        pxt = []
-        pxt.append((6 + (4 * ((4 - ch) * 2))))
-        pxt.append(0)
-        pxt.append(0)
-        pxt.append((tmp2 & 255))
-        pxt.append(((int(tmp2) >> int(8)) & 255))
-        mb.i2c.write(self.addr,bytes(pxt))
+def init(addr=64):
+  global address
+  address = addr
+  i2c.write(address,bytes([0,0]))
+  oldmode = (i2c.read(address,1))[0]
+  newmode = ((oldmode & 127) | 16)
+  i2c.write(address,bytes([0,newmode]))
+  i2c.write(address,bytes([254,121]))
+  i2c.write(address,bytes([0,oldmode]))
+  mb.sleep(5)
+  i2c.write(address,bytes([0,(oldmode | 161)]))
+        
+def servo(ch, deg):
+  if address is None:
+    init(64)
+  tmp = round(((((deg * 10) + 600) * 4095) / 20000))
+  pxt = []
+  pxt.append((6 + (4 * (16 - ch))))
+  pxt.append(0)
+  pxt.append(0)
+  pxt.append((tmp & 255))
+  pxt.append(((int(tmp) >> int(8)) & 255))
+  i2c.write(address,bytes(pxt))
+
+def motor(ch, speed):
+  if address is None:
+    init(64)
+  tmp = speed * 16
+  tmp = min(tmp,4095)
+  tmp = max(tmp,-4095)
+  tmp2 = 0
+  if (tmp < 0):
+    tmp2 = -tmp
+    tmp = 0
+  pxt = []
+  pxt.append((6 + (4 * (((4 - ch) * 2) + 1))))
+  pxt.append(0)
+  pxt.append(0)
+  pxt.append((tmp & 255))
+  pxt.append(((int(tmp) >> int(8)) & 255))
+  i2c.write(address,bytes(pxt))
+  pxt = []
+  pxt.append((6 + (4 * ((4 - ch) * 2))))
+  pxt.append(0)
+  pxt.append(0)
+  pxt.append((tmp2 & 255))
+  pxt.append(((int(tmp2) >> int(8)) & 255))
+  i2c.write(address,bytes(pxt))
--- Modules/usonic.py
+++ Modules/usonic.py
@@ -3,28 +3,33 @@
 
 import utime
 
-class HCSR04:
-    def __init__(self, trigger, echo):
-        self.trigger = trigger
-        self.echo = echo
-    def _usec_getecho(self):
-        count = 0
-        self.trigger.write_digital(1)
-        self.trigger.write_digital(0)
-        while self.echo.read_digital() == 0:
-            utime.sleep_us(1)
-            count += 1
-            if count > 1000:
-                return 0
-        t1 = utime.ticks_us()
-        count = 0
-        while self.echo.read_digital() > 0:
-            utime.sleep_us(1)
-            count += 1
-            if count > 1000:
-                return 0
-        t2 = utime.ticks_us()
-        return utime.ticks_diff(t2, t1)
-    def distance(self):
-        t = self._usec_getecho()
-        return int(340.0 * t / 20000)
+triggerP = None
+echoP = None
+
+def init(trigger, echo):
+  global triggerP, echoP
+  triggerP = trigger
+  echoP = echo
+
+def _usec_getecho():
+  c = 0
+  triggerP.write_digital(1)
+  triggerP.write_digital(0)
+  while echoP.read_digital() == 0:
+    utime.sleep_us(1)
+    c += 1
+    if c > 1000:
+      return 0
+  t1 = utime.ticks_us()
+  c = 0
+  while echoP.read_digital() > 0:
+    utime.sleep_us(1)
+    c += 1
+    if c > 1000:
+      return 0
+  t2 = utime.ticks_us()
+  return utime.ticks_diff(t2, t1)
+
+def distance():
+  t = _usec_getecho()
+  return int(340.0 * t / 20000)
